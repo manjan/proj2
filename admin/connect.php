28dc45c3149fe4352cbd297e48532bd6bfecbd41
Romee Prajapati <romee.prajapati@gmail.com>
2013-04-14 12:36:54 +0800

    DB configuration

--- admin/connect.php
+++ admin/connect.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpass = 'root';
+$dbpass = '';
 $dbh=mysql_connect($dbhost, $dbuser,$dbpass) or die('Cannot connect to the database because: ' . mysql_error());
 $dbname = 'hankook';
 mysql_select_db($dbname);
